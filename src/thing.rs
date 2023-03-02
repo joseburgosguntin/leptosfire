f46579eeb5f048054e583ad2a98f3c8d0682acf8
joseburgosguntin <josem.123burgos@gmail.com>
2023-03-02 11:19:37 -0400

    testing actions and server fns

---
+++ src/thing.rs
@@ -0,0 +1,14 @@
+// use leptos::*;
+// use serde::{Deserialize, Serialize};
+
+// #[derive(Debug, Clone, Serialize, Deserialize)]
+// pub struct Thing {
+//     pub name: String,
+// }
+
+// #[server(GetThing, "/api")]
+// pub async fn get_thing() -> Result<Thing, ServerFnError> {
+//     Ok(Thing {
+//         name: "thing".to_string(),
+//     })
+// }
